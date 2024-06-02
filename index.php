1ba9464c4a6f970edcc8ac327b37b83af7bd5ba3
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-06-02 14:48:23 +0200

    Added head

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Portfolio - Sacha Meunier</title>
+
+    <link rel="stylesheet" href="./dist/css/main.css">
+    <meta name="author" content="Sacha Meunier">
+    <meta name="description"
+          content="Bienvenue sur mon Portfolio en tant que web developer. Découvrez mes projets, ma formation et mes compétences.">
+    <meta name="keywords"
+          content="web developer, développement web, développeur web, développeur front-end, développeur back-end, développement d’applications web, UX, UI, UX/UI, HTML, CSS, JavaScript, PHP, Git, programmation">
+    <meta property="og:url" content="https://sacha-meunier.com">
+    <meta property="og:title" content="Portfolio - Sacha Meunier">
+    <meta property="og:type" content="website">
+    <meta property="og:description"
+          content="Bienvenue sur mon Portfolio en tant que web developer. Découvrez mes projets, ma formation et mes compétences.">
+    <meta property="og:image" content="/content/images/logo-menu.svg">
+    <meta property="og:image:alt" content="Le logo de Sacha Meunier"/>
+
+    <link rel="icon" type="image/svg+xml" href="./favicon.svg"/>
+    <link rel="alternate icon" href="/favicon.ico"/>
+</head>
