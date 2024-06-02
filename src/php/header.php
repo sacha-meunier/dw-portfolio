9c53bdd7bb1b127dbe98e668576634286f63ba6c
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-06-02 18:32:09 +0200

    Added header.php

---
+++ src/php/header.php
@@ -0,0 +1,30 @@
+<header class="header">
+    <div class="header__wrapper">
+        <h1 class="header__logo">
+            <a href="#">
+                <img class="header__logo-item" src="/content/images/header-logo.svg"
+                     alt="Zekiah - retour à l'accueil"
+                     itemprop="logo" width="112" height="40">
+            </a>
+        </h1>
+
+        <nav class="header__nav">
+            <h2 class="sro">Navigation</h2>
+            <ul class="header__nav-list">
+                <li class="header__nav-item"><a class="header__nav-link" href="#">Accueil</a></li>
+                <li class="header__nav-item"><a class="header__nav-link" href="#">Services</a></li>
+                <li class="header__nav-item"><a class="header__nav-link" href="#">Projets</a></li>
+                <li class="header__nav-item"><a class="header__nav-link" href="#">À propos</a></li>
+                <li class="header__nav-item"><a class="header__nav-link" href="#">Compétences</a></li>
+            </ul>
+        </nav>
+
+        <div class="header__buttons">
+            <a class="header__contact" href="#">Contact</a>
+            <div class="nav-toggler">
+                <span class="nav-toggler-line"></span>
+                <span class="nav-toggler-line"></span>
+            </div>
+        </div>
+    </div>
+</header>
