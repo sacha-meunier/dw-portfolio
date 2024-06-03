4e30eb2ebe28cc5230bfcc3bdfc5fa42ba6c1a46
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-06-03 19:31:55 +0200

    Improved accessibility

--- src/php/header.php
+++ src/php/header.php
@@ -1,14 +1,16 @@
-<header class="header">
+<header class="header" role="banner">
     <div class="header__wrapper">
-        <h1 class="header__logo">
-            <a href="#">
-                <img class="header__logo-item" src="/content/images/header-logo.svg"
-                     alt="Zekiah - retour à l'accueil"
-                     itemprop="logo" width="112" height="40">
+        <h1>
+            <a class="header__logo" href="#" aria-label="Zekiah homepage">
+                <svg class="header__logo-item" width="18" height="22" viewBox="0 0 18 22" fill="red"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <path d="M15.8574 0L0 4.06981L4.31441 5.91274L10.0291 4.33857L5.44978 12.5934L10.3319 22L17.3333 4.33857L13.2082 7.33333L10.0291 13.3229L9.23435 11.6719L15.8574 0Z"/>
+                </svg>
+                <span class="header__logo-text">Zekiah</span>
             </a>
         </h1>
 
-        <nav class="header__nav">
+        <nav class="header__nav" role="navigation" aria-label="Primary">
             <h2 class="sro">Navigation</h2>
             <ul class="header__nav-list">
                 <li class="header__nav-item"><a class="header__nav-link" href="#">Accueil</a></li>
@@ -22,8 +24,23 @@
         <div class="header__buttons">
             <a class="header__contact" href="#">Contact</a>
             <div class="nav-toggler">
-                <span class="nav-toggler-line"></span>
-                <span class="nav-toggler-line"></span>
+                <input class="nav-toggler__input" id="toggler" type="checkbox" aria-controls="primary-nav" aria-expanded="false"/>
+
+                <label class="nav-toggler__label" for="toggler" aria-label="Toggle navigation">
+                    <span class="nav-toggler__line"></span>
+                    <span class="nav-toggler__line"></span>
+                </label>
+
+                <nav class="nav-toggler__nav" id="primary-nav" role="navigation" aria-label="Mobile navigation">
+                    <h2 class="sro">Navigation</h2>
+                    <ul class="nav-toggler__list">
+                        <li class="nav-toggler__item"><a class="nav-toggler__link" href="#">Accueil</a></li>
+                        <li class="nav-toggler__item"><a class="nav-toggler__link" href="#">Services</a></li>
+                        <li class="nav-toggler__item"><a class="nav-toggler__link" href="#">Projets</a></li>
+                        <li class="nav-toggler__item"><a class="nav-toggler__link" href="#">À propos</a></li>
+                        <li class="nav-toggler__item"><a class="nav-toggler__link" href="#">Compétences</a></li>
+                    </ul>
+                </nav>
             </div>
         </div>
     </div>
