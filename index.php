dee82048374677af369088555633fb787923b67e
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-06-19 00:50:06 +0200

    Added base WP installation

--- index.php
+++ index.php
@@ -1,39 +1,17 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-    <title>Portfolio - Sacha Meunier</title>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-    <link rel="stylesheet" href="./dist/css/main.css">
-    <meta name="author" content="Sacha Meunier">
-    <meta name="description"
-          content="Bienvenue sur mon Portfolio en tant que web developer. Découvrez mes projets, ma formation et mes compétences.">
-    <meta name="keywords"
-          content="web developer, développement web, développeur web, développeur front-end, développeur back-end, développement d’applications web, UX, UI, UX/UI, HTML, CSS, JavaScript, PHP, Git, programmation">
-    <meta property="og:url" content="https://sacha-meunier.com">
-    <meta property="og:title" content="Portfolio - Sacha Meunier">
-    <meta property="og:type" content="website">
-    <meta property="og:description"
-          content="Bienvenue sur mon Portfolio en tant que web developer. Découvrez mes projets, ma formation et mes compétences.">
-    <meta property="og:image" content="/content/images/logo-menu.svg">
-    <meta property="og:image:alt" content="Le logo de Sacha Meunier"/>
-
-    <link rel="icon" type="image/svg+xml" href="./favicon.svg"/>
-    <link rel="alternate icon" href="/favicon.ico"/>
-</head>
-
-<body>
-<input id="scheme-color" type="checkbox" aria-label="hidden">
-
-<?php include 'src/php/header.php' ?>
-<main>
-<?php include 'src/php/contact.php' ?>
-<?php /*include 'src/php/project-item.php' */?>
-<?php /*include 'src/php/project.php' */?>
-</main>
-<?php include 'src/php/footer.php' ?>
-<script src="dist/js/main.js"></script>
-</body>
-</html>
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
---
+++ wp-content/themes/portfolio/index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Portfolio - Sacha Meunier</title>
+
+    <link rel="stylesheet" href="../../../dist/css/main.css">
+    <meta name="author" content="Sacha Meunier">
+    <meta name="description"
+          content="Bienvenue sur mon Portfolio en tant que web developer. Découvrez mes projets, ma formation et mes compétences.">
+    <meta name="keywords"
+          content="web developer, développement web, développeur web, développeur front-end, développeur back-end, développement d’applications web, UX, UI, UX/UI, HTML, CSS, JavaScript, PHP, Git, programmation">
+    <meta property="og:url" content="https://sacha-meunier.com">
+    <meta property="og:title" content="Portfolio - Sacha Meunier">
+    <meta property="og:type" content="website">
+    <meta property="og:description"
+          content="Bienvenue sur mon Portfolio en tant que web developer. Découvrez mes projets, ma formation et mes compétences.">
+    <meta property="og:image" content="/content/images/logo-menu.svg">
+    <meta property="og:image:alt" content="Le logo de Sacha Meunier"/>
+
+    <link rel="icon" type="image/svg+xml" href="../../../favicon.svg"/>
+    <link rel="alternate icon" href="/favicon.ico"/>
+</head>
+
+<body>
+<input id="scheme-color" type="checkbox" aria-label="hidden">
+
+<?php include 'src/php/header.php' ?>
+<main>
+<?php include 'src/php/contact.php' ?>
+<?php /*include 'src/php/project-item.php' */?>
+<?php /*include 'src/php/project.php' */?>
+</main>
+<?php include 'src/php/footer.php' ?>
+<script src="../../../dist/js/main.js"></script>
+</body>
+</html>
