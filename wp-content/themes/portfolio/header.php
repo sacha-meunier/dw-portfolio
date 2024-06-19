f665823ed69968b4bf9f2af1164c675d0ebbbea2
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-06-19 04:05:53 +0200

    Fixing links

--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -1,3 +1,30 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Portfolio - Sacha Meunier</title>
+    <link rel="stylesheet" href="../../../dist/css/main.css">
+    <meta name="author" content="Sacha Meunier">
+    <meta name="description"
+          content="Bienvenue sur mon Portfolio en tant que web developer. Découvrez mes projets, ma formation et mes compétences.">
+    <meta name="keywords"
+          content="web developer, développement web, développeur web, développeur front-end, développeur back-end, développement d’applications web, UX, UI, UX/UI, HTML, CSS, JavaScript, PHP, Git, programmation">
+    <meta property="og:url" content="https://sacha-meunier.com">
+    <meta property="og:title" content="Portfolio - Sacha Meunier">
+    <meta property="og:type" content="website">
+    <meta property="og:description"
+          content="Bienvenue sur mon Portfolio en tant que web developer. Découvrez mes projets, ma formation et mes compétences.">
+    <meta property="og:image" content="/content/images/logo-menu.svg">
+    <meta property="og:image:alt" content="Le logo de Sacha Meunier"/>
+
+    <link rel="icon" type="image/svg+xml" href="../../../favicon.svg"/>
+    <link rel="alternate icon" href="/favicon.ico"/>
+</head>
+
+<body>
+<input id="scheme-color" type="checkbox" aria-label="hidden">
 <header class="header" role="banner">
     <div class="header__wrapper">
         <h1>
@@ -15,7 +42,7 @@
             <ul class="header__nav-list">
                 <li class="header__nav-item"><a class="header__nav-link" href="<?= esc_url(home_url('/')); ?>">Accueil</a></li>
                 <li class="header__nav-item"><a class="header__nav-link" href="<?= esc_url(home_url('/')); ?>#services">Services</a></li>
-                <li class="header__nav-item"><a class="header__nav-link" href="<?= esc_url(home_url('/')); ?>#project">Projets</a></li>
+                <li class="header__nav-item"><a class="header__nav-link" href="<?= esc_url(get_projects_page_url()); ?>">Projets</a></li>
                 <li class="header__nav-item"><a class="header__nav-link" href="<?= esc_url(home_url('/')); ?>#training">Formations</a></li>
             </ul>
         </nav>
@@ -34,14 +61,14 @@
                 <nav class="nav-toggler__nav" id="primary-nav" role="navigation" aria-label="Mobile navigation">
                     <h2 class="sro">Navigation</h2>
                     <ul class="nav-toggler__list">
-                        <li class="nav-toggler__item"><a class="nav-toggler__link" href="#">Accueil</a></li>
-                        <li class="nav-toggler__item"><a class="nav-toggler__link" href="#">Services</a></li>
-                        <li class="nav-toggler__item"><a class="nav-toggler__link" href="#">Projets</a></li>
-                        <li class="nav-toggler__item"><a class="nav-toggler__link" href="#">À propos</a></li>
-                        <li class="nav-toggler__item"><a class="nav-toggler__link" href="#">Compétences</a></li>
+                        <li class="nav-toggler__item"><a class="nav-toggler__link" href="<?= esc_url(home_url('/')); ?>">Accueil</a></li>
+                        <li class="nav-toggler__item"><a class="nav-toggler__link" href="<?= esc_url(home_url('/')); ?>#services">Services</a></li>
+                        <li class="nav-toggler__item"><a class="nav-toggler__link" href="<?= esc_url(get_projects_page_url()); ?>">Projets</a></li>
+                        <li class="nav-toggler__item"><a class="nav-toggler__link" href="<?= esc_url(home_url('/')); ?>#formations">Formations</a></li>
                     </ul>
                 </nav>
             </div>
         </div>
     </div>
 </header>
+<main>
