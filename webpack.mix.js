b64ca4c06025ebaa9da4f2521fbcc265982f3b6e
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-06-19 03:16:38 +0200

    Fixed linked

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,8 +2,8 @@
 
 let mix = require('laravel-mix');
 
-mix.ts('src/ts/main.ts', 'js/main.js').sourceMaps()
-    .sass('src/scss/main.scss', 'css').sourceMaps()
+mix.ts('wp-content/themes/portfolio/public/src/ts/main.ts', 'js/main.js').sourceMaps()
+    .sass('wp-content/themes/portfolio/public/src/scss/main.scss', 'css').sourceMaps()
     .setPublicPath('dist').setResourceRoot('../')
     .browserSync({
         proxy: false,
