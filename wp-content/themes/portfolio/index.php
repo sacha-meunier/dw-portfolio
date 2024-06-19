3a77eb641681c3839fa054aaa07064dc267d5804
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-06-19 05:41:23 +0200

    Update index.php

--- wp-content/themes/portfolio/index.php
+++ wp-content/themes/portfolio/index.php
@@ -64,39 +64,42 @@
         <h2 class="section__heading">Projets récents</h2>
         <p class="section__copy">Projets scolaires, personnels et professionnels</p>
         <div class="project__wrapper">
-            <?php
-            $projects = new WP_Query(array(
-                'post_type' => 'project',
-                'posts_per_page' => -1
-            ));
-
-            if ($projects->have_posts()):
-                while ($projects->have_posts()): $projects->the_post();
-                    $project_image = get_the_post_thumbnail_url(get_the_ID(), 'large');
-                    ?>
-                    <a class="project__card" href="<?php the_permalink(); ?>">
-                        <div class="project__card__figure">
-                            <?php if ($project_image): ?>
-                                <img class="project__card__image" src="<?= esc_url($project_image); ?>"
-                                     alt="<?php the_title(); ?>">
-                            <?php else: ?>
-                                <img class="project__card__image"
-                                     src="<?= esc_url(get_template_directory_uri()); ?>/path/to/default-image.jpg"
-                                     alt="<?php the_title(); ?>">
-                            <?php endif; ?>
-                        </div>
-                        <div class="project__card__body">
-                            <div class="project__card__title"><?php the_title(); ?></div>
-                            <div class="project__card__copy"><?php the_excerpt(); ?></div>
-                        </div>
-                    </a>
-                <?php
-                endwhile;
-                wp_reset_postdata(); // Réinitialiser les données de la boucle
-            else:
-                ?>
-                <p>Aucun projet trouvé.</p>
-            <?php endif; ?>
+            <div class="project__card">
+                <div class="project__card__figure">
+                    <img class="project__card__image" src="" alt="">
+                </div>
+                <div class="project__card__body">
+                    <div class="project__card__title">Le Sef de Huy</div>
+                    <div class="project__card__copy">Création d’un site web pour une asbl dans le cadre du cours de Design
+                        Web.
+                    </div>
+                </div>
+            </div>
+            <div class="project__card project__card--reversed">
+                <div class="project__card__figure">
+                    <img class="project__card__image" src="" alt="">
+                </div>
+                <div class="project__card__body">
+                    <div class="project__card__title">Le CV</div>
+                    <div class="project__card__copy">Projet consistant à cloner la page d’accueil d’un site et la
+                        transformer en un CV dans le cadre du cours de Design Web.
+                    </div>
+                </div>
+            </div>
+            <div class="project__card">
+                <div class="project__card__figure">
+                    <img class="project__card__image" src="" alt="">
+                </div>
+                <div class="project__card__body">
+                    <div class="project__card__title">Le Portfolio</div>
+                    <div class="project__card__copy">Création d’un Portfolio utilisant Wordpress dans le cadre du cours de
+                        Design Web.
+                    </div>
+                </div>
+            </div>
+            <a class="button" type="submit" href="<?= esc_url(get_projects_page_url()); ?>">
+                Voir mes projets
+            </a>
         </div>
     </section>
     <section class="services training" id="training">
