299e5c408fa3341396ed03edbe31f51c0f2011b1
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-06-19 04:06:37 +0200

    Removing useless code

--- wp-content/themes/portfolio/page-contact.php
+++ wp-content/themes/portfolio/page-contact.php
@@ -1,36 +1,7 @@
 <?php
 /* Template Name: Page de Contact */
 ?>
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Portfolio - Sacha Meunier</title>
-    <link rel="stylesheet" href="../../../dist/css/main.css">
-    <meta name="author" content="Sacha Meunier">
-    <meta name="description"
-          content="Bienvenue sur mon Portfolio en tant que web developer. Découvrez mes projets, ma formation et mes compétences.">
-    <meta name="keywords"
-          content="web developer, développement web, développeur web, développeur front-end, développeur back-end, développement d’applications web, UX, UI, UX/UI, HTML, CSS, JavaScript, PHP, Git, programmation">
-    <meta property="og:url" content="https://sacha-meunier.com">
-    <meta property="og:title" content="Portfolio - Sacha Meunier">
-    <meta property="og:type" content="website">
-    <meta property="og:description"
-          content="Bienvenue sur mon Portfolio en tant que web developer. Découvrez mes projets, ma formation et mes compétences.">
-    <meta property="og:image" content="/content/images/logo-menu.svg">
-    <meta property="og:image:alt" content="Le logo de Sacha Meunier"/>
-
-    <link rel="icon" type="image/svg+xml" href="../../../favicon.svg"/>
-    <link rel="alternate icon" href="/favicon.ico"/>
-</head>
-
-<body>
-<input id="scheme-color" type="checkbox" aria-label="hidden">
 <?php get_header(); ?>
-
-<main>
     <section class="contact-page">
         <h1 class="contact-page__title">Donnons vie à vos projets dès maintenant</h1>
         <div class="contact-page__content">
@@ -85,9 +56,4 @@
             </aside>
         </div>
     </section>
-
-</main>
 <?php get_footer(); ?>
-<script src="../../../dist/js/main.js"></script>
-</body>
-</html>
