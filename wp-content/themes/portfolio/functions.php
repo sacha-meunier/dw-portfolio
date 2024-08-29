144f93b318942375b9604c1f0018b883f6fce761
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-08-29 17:26:17 +0200

    Imported new functions

--- wp-content/themes/portfolio/functions.php
+++ wp-content/themes/portfolio/functions.php
@@ -1,7 +1,9 @@
 <?php
 
+require_once(__DIR__ . '/includes/asset-public.php');
 require_once( __DIR__ . '/includes/disable-gutenberg.php');
-require_once( __DIR__ . '/includes/public-path.php');
+require_once( __DIR__ . '/includes/admin-bar.php');
+require_once( __DIR__ . '/includes/title-tag.php');
 require_once( __DIR__ . '/includes/get-page-url.php');
 require_once( __DIR__ . '/includes/project-post-type.php');
-/*require_once( __DIR__ . '/includes/load-theme-assets.php');* /* wp_head ads unnecessary CSS */
+/*require_once( __DIR__ . '/includes/asset-theme.php');* /* wp_head ads unnecessary CSS */
