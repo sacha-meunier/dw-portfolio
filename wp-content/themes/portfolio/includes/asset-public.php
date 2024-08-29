6ee3c252fb89bc1cd44339738988e392f314f326
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-08-29 17:25:24 +0200

    Renamed file

--- wp-content/themes/portfolio/includes/asset-public.php
+++ wp-content/themes/portfolio/includes/asset-public.php
@@ -1,6 +1,7 @@
 <?php
 
-// Load a "public" file
+// Load a file from "public" folder
+// Usage : <?= dw_asset('theme.css');
 function dw_asset(string $file): string
 {
     return get_template_directory_uri() . '/public/' . $file;
