6782cc34bda8dc9f1d597d6d5ecc302c297da580
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-08-30 12:24:46 +0200

    Used custom fields

--- wp-content/themes/portfolio/single-project.php
+++ wp-content/themes/portfolio/single-project.php
@@ -33,6 +33,7 @@ if (function_exists('get_field')) {
 
         $project_card4_title = get_field('project_card4_title');
         $project_card4_copy = get_field('project_card4_copy');
+        $project_card4_img = get_field('project_card4_img');
         $project_card4_cta = get_field('project_card4_cta');
         $project_card4_cta_url = get_field('project_card4_cta_url');
         $project_cta = get_field('project_cta');
@@ -50,106 +51,92 @@ if (function_exists('get_field')) {
                 <path d="M1 11V1M1 1H11M1 1L11 11" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
 
-            <a class="project-item__breadcrumb-text" href="<?= esc_url(home_url('/projects')); ?>">Retour aux projets</a>
+            <a class="project-item__breadcrumb-text" href="<?= esc_url(home_url('/projects')); ?>"><?= $project_ariane; ?></a>
         </div>
 
-        <a class="project-item__live" href="<?php the_field('project_url'); ?>">
+        <a class="project-item__live" href="<?= esc_url($project_url); ?>">
             <div class="project-item__live-subtitle">
-                <span class="project-item__live-text">Voir en direct</span>
+                <span class="project-item__live-text"> <?= $project_subtitle; ?></span>
                 <svg class="project-item__live-img" width="12" height="11" viewBox="0 0 9 8" fill="none" stroke="#52525B"
                      xmlns="http://www.w3.org/2000/svg">
                     <path d="M1.58337 1.08301H7.41671M7.41671 1.08301V6.91634M7.41671 1.08301L1.58337 6.91634"
                           stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
             </div>
-            <span class="project-item__live-url"><?php the_field('project_url'); ?></span>
+            <span class="project-item__live-url"><?= esc_html($project_url); ?></span>
         </a>
 
         <div class="project-item__figure">
-            <img class="project-item__figure-img" src="<?php the_field('project_image'); ?>" alt="">
+            <img class="project-item__figure-img" src="<?= esc_url($project_img); ?>" alt="Image du projet">
         </div>
 
         <div class="project-item__wrapper">
 
             <div class="project-item__card">
                 <div class="project-item__card__body">
-                    <div class="project-item__card__title"><?php the_field('project_description_title'); ?></div>
-                    <div class="project-item__card__copy"><?php the_field('project_description_copy'); ?></div>
+                    <div class="project-item__card__title"><?= esc_html($project_card1_title1); ?></div>
+                    <div class="project-item__card__copy"><?= esc_html($project_card1_copy1); ?></div>
                 </div>
 
                 <div class="project-item__card__body">
-                    <div class="project-item__card__title">Quel est le site à cloner ?</div>
-                    <div class="project-item__card__copy"><?php the_field('cloning_site'); ?></div>
+                    <div class="project-item__card__title"><?= esc_html($project_card1_title2); ?></div>
+                    <div class="project-item__card__copy"><?= esc_html($project_card1_copy2); ?></div>
                 </div>
 
                 <div class="project-item__card__body">
-                    <div class="project-item__card__title">Pourquoi <?php the_field('cloning_site_name'); ?> ?</div>
-                    <div class="project-item__card__copy"><?php the_field('cloning_reason'); ?></div>
+                    <div class="project-item__card__title"><?= esc_html($project_card1_title3); ?></div>
+                    <div class="project-item__card__copy"><?= esc_html($project_card1_copy3); ?></div>
                 </div>
             </div>
 
             <div class="project-item__card project-item__card--details">
                 <div class="project-item__card__body">
-                    <div class="project-item__card__title">Détails</div>
+                    <div class="project-item__card__title"><?= esc_html($project_card2_title); ?></div>
                     <div class="project-item__card__group">
                         <div class="project-item__card__group-content">
-                            <div class="project-item__card__group-label">Année</div>
+                            <div class="project-item__card__group-label"><?= esc_html($project_card2_label1); ?></div>
                             <div class="project-item__card__group-value">
-                                <div class="project-item__card__group-item"><?php the_field('project_year'); ?></div>
+                                <div class="project-item__card__group-item"><?= esc_html($project_card2_value1); ?></div>
                             </div>
                         </div>
                         <div class="project-item__card__group-content">
-                            <div class="project-item__card__group-label">Projet</div>
+                            <div class="project-item__card__group-label"><?= esc_html($project_card2_label2); ?></div>
                             <div class="project-item__card__group-value">
-                                <div class="project-item__card__group-item"><?php the_field('project_type'); ?></div>
+                                <div class="project-item__card__group-item"><?= esc_html($project_card2_value2); ?></div>
                             </div>
                         </div>
                         <div class="project-item__card__group-content">
-                            <div class="project-item__card__group-label">Services</div>
+                            <div class="project-item__card__group-label"><?= esc_html($project_card2_label3); ?></div>
                             <div class="project-item__card__group-value">
-                                <div class="project-item__card__group-item"><?php the_field('project_services'); ?></div>
+                                <div class="project-item__card__group-item"><?= esc_html($project_card2_value3); ?></div>
                             </div>
                         </div>
                         <div class="project-item__card__group-content">
-                            <div class="project-item__card__group-label">Technologie</div>
+                            <div class="project-item__card__group-label"><?= esc_html($project_card2_label4); ?></div>
                             <div class="project-item__card__group-value">
-                                <div class="project-item__card__group-item"><?php the_field('project_technology'); ?></div>
+                                <div class="project-item__card__group-item"><?= esc_html($project_card2_value4); ?></div>
                             </div>
                         </div>
                         <div class="project-item__card__group-content">
-                            <div class="project-item__card__group-label">Outils</div>
+                            <div class="project-item__card__group-label"><?= esc_html($project_card2_label5); ?></div>
                             <div class="project-item__card__group-value">
-                                <div class="project-item__card__group-item"><?php the_field('project_tool'); ?></div>
+                                <div class="project-item__card__group-item"><?= esc_html($project_card2_value5); ?></div>
                             </div>
                         </div>
-                        <!--<div class="project-item__card__group-content">
-                            <div class="project-item__card__group-label">Services</div>
-                            <div class="project-item__card__group-value">
-                                <?php
-/*                                if (have_rows('project_services')):
-                                    while (have_rows('project_services')): the_row();
-                                        */?>
-                                        <div class="project-item__card__group-item"><?php /*the_sub_field('service'); */?></div>
-                                    <?php
-/*                                    endwhile;
-                                endif;
-                                */?>
-                            </div>
-                        </div>-->
                     </div>
                 </div>
             </div>
 
             <div class="project-item__card project-item__card--grid">
                 <div class="project-item__card__body">
-                    <div class="project-item__card__title">Version en ligne</div>
-                    <div class="project-item__card__copy"><?php the_field('live_version_description'); ?></div>
+                    <div class="project-item__card__title"><?= esc_html($project_card3_title); ?></div>
+                    <div class="project-item__card__copy"><?= esc_html($project_card3_copy); ?></div>
                 </div>
                 <div class="project-item__card__figure">
-                    <img class="project-item__card__image" src="<?php the_field('live_version_image'); ?>" alt="">
+                    <img class="project-item__card__image" src="<?= esc_url($project_card3_img); ?>" alt="">
                 </div>
-                <a class="button button--in-card project-item__card__link" href="<?php the_field('live_version_url'); ?>">
-                    Voir en direct
+                <a class="button button--in-card project-item__card__link" href="<?= esc_url($project_url); ?>">
+                    <?= $project_card3_cta; ?>
                     <svg class="project-item__live-img" width="12" height="11" viewBox="0 0 9 8" fill="none" stroke="#52525B"
                          xmlns="http://www.w3.org/2000/svg">
                         <path d="M1.58337 1.08301H7.41671M7.41671 1.08301V6.91634M7.41671 1.08301L1.58337 6.91634"
@@ -160,14 +147,14 @@ if (function_exists('get_field')) {
 
             <div class="project-item__card project-item__card--grid">
                 <div class="project-item__card__body">
-                    <div class="project-item__card__title">Vers Github</div>
-                    <div class="project-item__card__copy"><?php the_field('github_description'); ?></div>
+                    <div class="project-item__card__title"><?= esc_html($project_card4_title); ?></div>
+                    <div class="project-item__card__copy"><?= esc_html($project_card4_copy); ?></div>
                 </div>
                 <div class="project-item__card__figure">
-                    <img class="project-item__card__image" src="<?php the_field('github_image'); ?>" alt="">
+                    <img class="project-item__card__image" src="<?= esc_url($project_card4_img); ?>" alt="">
                 </div>
-                <a class="button button--in-card project-item__card__link" href="<?php the_field('github_url'); ?>">
-                    Vers Github
+                <a class="button button--in-card project-item__card__link" href="<?= esc_url($project_card4_cta_url); ?>">
+                    <?= $project_card4_cta; ?>
                     <svg class="project-item__live-img" width="12" height="11" viewBox="0 0 9 8" fill="none" stroke="#52525B"
                          xmlns="http://www.w3.org/2000/svg">
                         <path d="M1.58337 1.08301H7.41671M7.41671 1.08301V6.91634M7.41671 1.08301L1.58337 6.91634"
@@ -178,7 +165,7 @@ if (function_exists('get_field')) {
 
             <div class="project-item__card__buttons">
                 <a class="button project-item__card__link" href="<?= esc_url(get_contact_page_url()); ?>">
-                    Contactez-moi
+                    <?= $project_cta; ?>
                 </a>
 
                 <div class="project-item__breadcrumb">
@@ -187,7 +174,7 @@ if (function_exists('get_field')) {
                         <path d="M1 11V1M1 1H11M1 1L11 11" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
 
-                    <a class="project-item__breadcrumb-text" href="<?= esc_url(home_url('/projects')); ?>">Retour aux projets</a>
+                    <a class="project-item__breadcrumb-text" href="<?= esc_url(home_url('/projects')); ?>"><?= $project_ariane; ?></a>
                 </div>
             </div>
         </div>
