fb737c1bbf6e179773fb9b5454fa58e76675eae5
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-08-31 19:32:33 +0200

    Added images and animations

--- wp-content/themes/portfolio/project/card.php
+++ wp-content/themes/portfolio/project/card.php
@@ -1,6 +1,6 @@
 <a class="project__card" data-animate="slide-left-fade" href="<?php the_permalink(); ?>">
     <div class="project__card__figure">
-            <img class="project__card__image" data-animate="fade-in" src="#" alt="<?php the_title(); ?>">
+            <img class="project__card__image" data-animate="fade-in" src="<?= get_field('project_image'); ?>" alt="<?php the_title(); ?>">
     </div>
     <div class="project__card__body">
         <div class="project__card__title" data-animate="slide-left-fade"><?php the_title(); ?></div>
--- wp-content/themes/portfolio/single-project.php
+++ wp-content/themes/portfolio/single-project.php
@@ -6,7 +6,8 @@ if (function_exists('get_field')) {
         $project_ariane = get_field('project_ariane');
         $project_subtitle = get_field('project_subtitle');
         $project_url = get_field('project_url');
-        $project_img = get_field('project_img');
+        $project_img = get_field('project_image');
+        $project_img_mac = get_field('project_image_mac');
         $project_card1_title1 = get_field('project_card1_title1');
         $project_card1_copy1 = get_field('project_card1_copy1');
         $project_card1_title2 = get_field('project_card1_title2');
@@ -43,9 +44,9 @@ if (function_exists('get_field')) {
 
 <main class="main-content">
     <section class="project-item">
-        <h2 class="project-item__heading"><?= the_title(); ?></h2>
+        <h2 class="project-item__heading" data-animate="slide-up-fade"><?= the_title(); ?></h2>
 
-        <div class="project-item__breadcrumb">
+        <div class="project-item__breadcrumb" data-animate="fade-in">
             <svg class="project-item__breadcrumb-img" width="12" height="12" viewBox="0 0 12 12" fill="none"
                  stroke="#D4D4D8" xmlns="http://www.w3.org/2000/svg">
                 <path d="M1 11V1M1 1H11M1 1L11 11" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -56,70 +57,70 @@ if (function_exists('get_field')) {
 
         <a class="project-item__live" href="<?= esc_url($project_url); ?>">
             <div class="project-item__live-subtitle">
-                <span class="project-item__live-text"> <?= $project_subtitle; ?></span>
-                <svg class="project-item__live-img" width="12" height="11" viewBox="0 0 9 8" fill="none" stroke="#52525B"
+                <span class="project-item__live-text" data-animate="slide-up-fade"> <?= $project_subtitle; ?></span>
+                <svg class="project-item__live-img" data-animate="slide-up-fade" width="12" height="11" viewBox="0 0 9 8" fill="none" stroke="#52525B"
                      xmlns="http://www.w3.org/2000/svg">
                     <path d="M1.58337 1.08301H7.41671M7.41671 1.08301V6.91634M7.41671 1.08301L1.58337 6.91634"
                           stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
             </div>
-            <span class="project-item__live-url"><?= esc_html($project_url); ?></span>
+            <span class="project-item__live-url" data-animate="slide-up-fade"><?= esc_html($project_url); ?></span>
         </a>
 
         <div class="project-item__figure">
-            <img class="project-item__figure-img" src="<?= esc_url($project_img); ?>" alt="Image du projet">
+            <img class="project-item__figure-img" data-animate="slide-up-fade" src="<?= esc_url($project_img_mac); ?>" alt="Image du projet">
         </div>
 
         <div class="project-item__wrapper">
 
-            <div class="project-item__card">
+            <div class="project-item__card" data-animate="slide-up-fade">
                 <div class="project-item__card__body">
-                    <div class="project-item__card__title"><?= esc_html($project_card1_title1); ?></div>
-                    <div class="project-item__card__copy"><?= esc_html($project_card1_copy1); ?></div>
+                    <div class="project-item__card__title" data-animate="slide-left-fade"><?= esc_html($project_card1_title1); ?></div>
+                    <div class="project-item__card__copy" data-animate="slide-left-fade"><?= esc_html($project_card1_copy1); ?></div>
                 </div>
 
                 <div class="project-item__card__body">
-                    <div class="project-item__card__title"><?= esc_html($project_card1_title2); ?></div>
-                    <div class="project-item__card__copy"><?= esc_html($project_card1_copy2); ?></div>
+                    <div class="project-item__card__title" data-animate="slide-left-fade"><?= esc_html($project_card1_title2); ?></div>
+                    <div class="project-item__card__copy" data-animate="slide-left-fade"><?= esc_html($project_card1_copy2); ?></div>
                 </div>
 
                 <div class="project-item__card__body">
-                    <div class="project-item__card__title"><?= esc_html($project_card1_title3); ?></div>
-                    <div class="project-item__card__copy"><?= esc_html($project_card1_copy3); ?></div>
+                    <div class="project-item__card__title" data-animate="slide-left-fade"><?= esc_html($project_card1_title3); ?></div>
+                    <div class="project-item__card__copy" data-animate="slide-left-fade"><?= esc_html($project_card1_copy3); ?></div>
                 </div>
             </div>
 
-            <div class="project-item__card project-item__card--details">
+            <div class="project-item__card project-item__card--details" data-animate="slide-up-fade">
                 <div class="project-item__card__body">
                     <div class="project-item__card__title"><?= esc_html($project_card2_title); ?></div>
                     <div class="project-item__card__group">
                         <div class="project-item__card__group-content">
-                            <div class="project-item__card__group-label"><?= esc_html($project_card2_label1); ?></div>
-                            <div class="project-item__card__group-value">
+                            <div class="project-item__card__group-label" data-animate="slide-left-fade"><?= esc_html($project_card2_label1); ?></div>
+                            <div class="project-item__card__group-value" data-animate="slide-left-fade">
                                 <div class="project-item__card__group-item"><?= esc_html($project_card2_value1); ?></div>
                             </div>
                         </div>
                         <div class="project-item__card__group-content">
-                            <div class="project-item__card__group-label"><?= esc_html($project_card2_label2); ?></div>
-                            <div class="project-item__card__group-value">
+                            <div class="project-item__card__group-label" data-animate="slide-left-fade"><?= esc_html($project_card2_label2); ?></div>
+                            <div class="project-item__card__group-value" data-animate="slide-left-fade">
                                 <div class="project-item__card__group-item"><?= esc_html($project_card2_value2); ?></div>
                             </div>
                         </div>
                         <div class="project-item__card__group-content">
-                            <div class="project-item__card__group-label"><?= esc_html($project_card2_label3); ?></div>
-                            <div class="project-item__card__group-value">
+                            <div class="project-item__card__group-label" data-animate="slide-left-fade"><?= esc_html($project_card2_label3); ?></div>
+                            <div class="project-item__card__group-value" data-animate="slide-left-fade">
                                 <div class="project-item__card__group-item"><?= esc_html($project_card2_value3); ?></div>
                             </div>
                         </div>
                         <div class="project-item__card__group-content">
-                            <div class="project-item__card__group-label"><?= esc_html($project_card2_label4); ?></div>
-                            <div class="project-item__card__group-value">
+                            <div class="project-item__card__group-label" data-animate="slide-left-fade"><?= esc_html($project_card2_label4); ?></div>
+                            <div class="project-item__card__group-value" data-animate="slide-left-fade">
                                 <div class="project-item__card__group-item"><?= esc_html($project_card2_value4); ?></div>
                             </div>
                         </div>
                         <div class="project-item__card__group-content">
-                            <div class="project-item__card__group-label"><?= esc_html($project_card2_label5); ?></div>
-                            <div class="project-item__card__group-value">
+                            <div class="project-item__card__group-label" data-animate="slide-left-fade"><?= esc_html($project_card2_label5); ?></div>
+                            <div class="project-item__card__group-value" data-animate="slide-left-fade">
                                 <div class="project-item__card__group-item"><?= esc_html($project_card2_value5); ?></div>
                             </div>
                         </div>
@@ -127,15 +128,15 @@ if (function_exists('get_field')) {
                 </div>
             </div>
 
-            <div class="project-item__card project-item__card--grid">
+            <div class="project-item__card project-item__card--grid" data-animate="slide-up-fade">
                 <div class="project-item__card__body">
-                    <div class="project-item__card__title"><?= esc_html($project_card3_title); ?></div>
-                    <div class="project-item__card__copy"><?= esc_html($project_card3_copy); ?></div>
+                    <div class="project-item__card__title" data-animate="slide-left-fade"><?= esc_html($project_card3_title); ?></div>
+                    <div class="project-item__card__copy" data-animate="slide-left-fade"><?= esc_html($project_card3_copy); ?></div>
                 </div>
                 <div class="project-item__card__figure">
-                    <img class="project-item__card__image" src="<?= esc_url($project_card3_img); ?>" alt="">
+                    <img class="project-item__card__image" data-animate="fade-in"src="<?= esc_url($project_img); ?>" alt="">
                 </div>
-                <a class="button button--in-card project-item__card__link" href="<?= esc_url($project_url); ?>">
+                <a class="button button--in-card project-item__card__link" data-animate="slide-left-fade" href="<?= esc_url($project_url); ?>">
                     <?= $project_card3_cta; ?>
                     <svg class="project-item__live-img" width="12" height="11" viewBox="0 0 9 8" fill="none" stroke="#52525B"
                          xmlns="http://www.w3.org/2000/svg">
@@ -145,15 +146,15 @@ if (function_exists('get_field')) {
                 </a>
             </div>
 
-            <div class="project-item__card project-item__card--grid">
+            <div class="project-item__card project-item__card--grid" data-animate="slide-up-fade">
                 <div class="project-item__card__body">
-                    <div class="project-item__card__title"><?= esc_html($project_card4_title); ?></div>
-                    <div class="project-item__card__copy"><?= esc_html($project_card4_copy); ?></div>
+                    <div class="project-item__card__title" data-animate="slide-left-fade"><?= esc_html($project_card4_title); ?></div>
+                    <div class="project-item__card__copy" data-animate="slide-left-fade"><?= esc_html($project_card4_copy); ?></div>
                 </div>
                 <div class="project-item__card__figure">
-                    <img class="project-item__card__image" src="<?= esc_url($project_card4_img); ?>" alt="">
+                    <img class="project-item__card__image" data-animate="fade-in"src="<?= esc_url($project_card4_img); ?>" alt="">
                 </div>
-                <a class="button button--in-card project-item__card__link" href="<?= esc_url($project_card4_cta_url); ?>">
+                <a class="button button--in-card project-item__card__link" data-animate="slide-left-fade" href="<?= esc_url($project_card4_cta_url); ?>">
                     <?= $project_card4_cta; ?>
                     <svg class="project-item__live-img" width="12" height="11" viewBox="0 0 9 8" fill="none" stroke="#52525B"
                          xmlns="http://www.w3.org/2000/svg">
@@ -164,11 +165,11 @@ if (function_exists('get_field')) {
             </div>
 
             <div class="project-item__card__buttons">
-                <a class="button project-item__card__link" href="<?= esc_url(get_contact_page_url()); ?>">
+                <a class="button project-item__card__link" data-animate="slide-left-fade" href="<?= esc_url(get_contact_page_url()); ?>">
                     <?= $project_cta; ?>
                 </a>
 
-                <div class="project-item__breadcrumb">
+                <div class="project-item__breadcrumb" data-animate="slide-left-fade">
                     <svg class="project-item__breadcrumb-img" width="12" height="12" viewBox="0 0 12 12" fill="none"
                          stroke="#D4D4D8" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1 11V1M1 1H11M1 1L11 11" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
